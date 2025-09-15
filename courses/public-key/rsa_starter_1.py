cf7bb31a9fc8ae2cf94e26d7c1f89665be0a080f
SPAS <spassystem2023@gmail.com>
2025-09-15 15:24:40 +0000

    public

---
+++ courses/public-key/rsa_starter_1.py
@@ -0,0 +1,9 @@
+#pow(base, exponent, modulus)
+base = 101
+exponent = 17
+modulus = 22663
+
+
+result = pow(base, exponent, modulus)
+
+print(result)
---
+++ courses/public-key/rsa_starter_2.py
@@ -0,0 +1,16 @@
+#N the modulus N = p * q
+# e the exponent
+
+#"Encrypt" the number 
+# 12 using the exponent 
+# e=65537 and the primes 
+# p=17 and  q = 23 . What number do you get as the ciphertext?
+
+
+p = 17
+
+q = 23
+
+e = 65537
+ 
+N = p * q
